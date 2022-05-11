c114834dd48dde8ef3811aea89cfa4235b4bf074
p <75785264+abottl@users.noreply.github.com>
2022-05-11 09:42:38 +0000

    ok

---
+++ quiz8/src/q8_helloCGU.js
@@ -0,0 +1,10 @@
+// hello CGU!
+const HelloCGU = () => {
+  return (<h1 style = {styleArgument}>hello CGU!</h1>);
+}
+const styleArgument = {
+  fontSize: '100px',
+  color: 'red'
+};
+
+export default HelloCGU;
